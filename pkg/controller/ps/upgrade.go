74f702ba37feac557d2cb4a001972d1cc05c7505
Sergey Chernomorets <chernomorets@yandex-team.ru>
2025-11-28 18:01:19 +0300

    skip smartUpdate when master is out of k8s

--- pkg/controller/ps/upgrade.go
+++ pkg/controller/ps/upgrade.go
@@ -2,6 +2,7 @@ package ps
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -83,6 +84,21 @@ func (r *PerconaServerMySQLReconciler) smartUpdate(ctx context.Context, sts *app
 	if err != nil {
 		return err
 	}
+
+	hh := strings.Split(primaryHost, ".")
+
+	foundPrimary := false
+	for _, pod := range pods.Items {
+		if hh[0] == pod.Name {
+			foundPrimary = true
+			break
+		}
+	}
+	if !foundPrimary {
+		log.Info("primaryHost  is not among pods", "primaryHost", primaryHost)
+		return nil
+	}
+
 	idx, err := getPodIndexFromHostname(primaryHost)
 	if err != nil {
 		return err
